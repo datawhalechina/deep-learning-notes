ca9393bd737cee69adcf45833a672108386f1c21
jshn9515 <jshn9510@gmail.com>
2026-08-05 13:51:34 -0400

    MNT: Fix print statement in clean_cache.py to include a period for consistency

--- utils/clean_cache.py
+++ utils/clean_cache.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parents[1]
 def main() -> None:
     for cache_dir in ROOT.rglob('*_files'):
         if cache_dir.is_dir():
-            print(f'Deleting: {cache_dir.name}', flush=True)
+            print(f'Deleting: {cache_dir.name}.', flush=True)
             shutil.rmtree(cache_dir)
 
 
