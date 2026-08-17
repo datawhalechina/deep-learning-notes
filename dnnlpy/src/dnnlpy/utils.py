d0bd5c499829e60672fab9bab3d123f74a190f4e
jshn9515 <jshn9510@gmail.com>
2026-08-16 23:34:05 -0400

    FEA: Add utility functions for byte conversion and update exports

--- dnnlpy/src/dnnlpy/utils.py
+++ dnnlpy/src/dnnlpy/utils.py
@@ -1,6 +1,13 @@
 import torch.nn as nn
 
-__all__ = ['count_params']
+GIB = 1024**3
+MIB = 1024**2
+
+__all__ = [
+    'bytes_to_gib',
+    'bytes_to_mib',
+    'count_params',
+]
 
 
 def count_params(module: nn.Module) -> int:
@@ -13,3 +20,27 @@ def count_params(module: nn.Module) -> int:
         num_params (int): The total number of parameters in the module.
     """
     return sum(param.numel() for param in module.parameters())
+
+
+def bytes_to_gib(num_bytes: float) -> float:
+    """Convert bytes to gibibytes (GiB).
+
+    Args:
+        num_bytes (float): The number of bytes to convert.
+
+    Returns:
+        GiB (float): The equivalent number of gibibytes (GiB).
+    """
+    return num_bytes / GIB
+
+
+def bytes_to_mib(num_bytes: float) -> float:
+    """Convert bytes to mebibytes (MiB).
+
+    Args:
+        num_bytes (float): The number of bytes to convert.
+
+    Returns:
+        MiB (float): The equivalent number of mebibytes (MiB).
+    """
+    return num_bytes / MIB
