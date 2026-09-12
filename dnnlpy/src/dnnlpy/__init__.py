f2069625c20a2b6994bd524aa7a5d1c91ee97fde
jshn9515 <jshn9510@gmail.com>
2026-09-12 18:38:39 -0400

    MNT: Add AVX2 and VNNI support check function in __init__.py

--- dnnlpy/src/dnnlpy/__init__.py
+++ dnnlpy/src/dnnlpy/__init__.py
@@ -1,3 +1,5 @@
+import torch
+
 from . import (
     cs336 as cs336,
     models as models,
@@ -19,3 +21,22 @@ from .utils import (
     bytes_to_mib as bytes_to_mib,
     count_params as count_params,
 )
+
+
+def is_avx2_vnni_2() -> bool:
+    """Check if the current CPU supports AVX2, VNNI, and related features.
+
+    See https://github.com/pytorch/pytorch/issues/148861 for more details.
+    Code ported from https://github.com/intel/torch-xpu-ops/pull/4341.
+    """
+    cap = torch.cpu.get_capabilities()
+    return (
+        cap.get('avx2', False)
+        and cap.get('avx_vnni', False)
+        and cap.get('avx_vnni_int8', False)
+        and cap.get('avx_ne_convert', False)
+        and not cap.get('avx512_f', False)
+    )
+
+
+IS_AVX2_VNNI_2 = is_avx2_vnni_2()
